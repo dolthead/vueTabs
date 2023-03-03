3286607208dd81ecb7492909a8baae96af8bd0f4
Todd H <github@simplybest.com>
2023-03-02 22:21:23 -0700

    add modal with onBeforeMount and onMounted

---
+++ src/components/SampleModal.vue
@@ -0,0 +1,37 @@
+<script lang="ts" setup>
+    import { modalController, IonPage, IonHeader, IonTitle, IonToolbar, IonButtons, IonButton, IonIcon, IonContent, IonCard, IonCardContent } from "@ionic/vue";
+    import { closeCircleOutline } from 'ionicons/icons';
+    import { onBeforeMount, onMounted } from "vue";
+
+    onBeforeMount(() => { // when you want to do something before the component is mounted
+        console.log('onBeforeMount');
+    });
+
+    onMounted(() => { // when you need access to the rendered DOM
+        console.log('onMounted');
+    });
+</script>
+
+<template>
+    <ion-page>
+        <ion-header>
+            <ion-toolbar>
+                <ion-title>
+                    Page info
+                </ion-title>
+                <ion-buttons slot="end" class="back-button">
+                    <ion-button @click="modalController.dismiss()">
+                        <ion-icon slot="icon-only" :icon="closeCircleOutline"></ion-icon>
+                    </ion-button>
+                </ion-buttons>
+            </ion-toolbar>
+        </ion-header>
+    <ion-content class="ion-padding">
+        <ion-card>
+            <ion-card-content>
+                Info goes here
+            </ion-card-content>
+        </ion-card>
+    </ion-content>
+    </ion-page>
+</template>
--- src/views/Tab1Page.vue
+++ src/views/Tab1Page.vue
@@ -1,27 +1,42 @@
+<script setup lang="ts">
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonButtons, IonFab, IonFabButton, IonIcon, modalController } from '@ionic/vue';
+import { chevronBackOutline } from 'ionicons/icons';
+import ExploreContainer from '@/components/ExploreContainer.vue';
+import SampleModal from '@/components/SampleModal.vue';
+
+const presentModal = async () => {
+  const modal = await modalController.create({
+      component: SampleModal,
+      breakpoints: [0, 0.65],
+      initialBreakpoint: 0.65,
+  });
+  return modal.present();
+}
+</script>
+
 <template>
   <ion-page>
     <ion-header>
       <ion-toolbar>
-        <ion-title>Tab 1</ion-title>
+          <ion-buttons slot="start">
+            <ion-fab>
+              <ion-fab-button size="small">
+                <ion-icon :icon="chevronBackOutline"></ion-icon>
+              </ion-fab-button>
+            </ion-fab>
+          </ion-buttons>
+        <ion-title style="margin-left: 40px">Tab 1</ion-title>
       </ion-toolbar>
     </ion-header>
     <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Tab 1</ion-title>
-        </ion-toolbar>
-      </ion-header>
 
       <ExploreContainer name="Tab 1 page" />
 
-      <ion-button :router-link="'/tabs/tab2'">Go to Tab2 (full path)</ion-button>
-      <ion-button :router-link="'tab2'">Go to Tab2 (relative path)</ion-button>
+      <ion-button :router-link="'/tabs/tab2'" expand="block">Go to Tab2 (full path)</ion-button>
+      <ion-button :router-link="'tab2'" expand="block">Go to Tab2 (relative path)</ion-button>
+      <br>
+      <ion-button @click="presentModal" expand="block">Open Modal</ion-button>
 
     </ion-content>
   </ion-page>
 </template>
-
-<script setup lang="ts">
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonButton } from '@ionic/vue';
-import ExploreContainer from '@/components/ExploreContainer.vue';
-</script>
